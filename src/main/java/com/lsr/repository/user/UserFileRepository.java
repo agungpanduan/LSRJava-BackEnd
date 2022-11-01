d51cb163c7788a48362d4770e52d4012178639f5
LAPTOP-J1CTKMRE\WOT <sourcecode.agung@gmail.com>
2022-11-01 13:23:58 +0700

    Add Repo

--- src/main/java/com/lsr/repository/user/UserFileRepository.java
+++ src/main/java/com/lsr/repository/user/UserFileRepository.java
@@ -3,7 +3,9 @@ package com.lsr.repository.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lsr.entity.user.UserFile;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserFileRepository extends JpaRepository<UserFile,Long> {
 
 	
